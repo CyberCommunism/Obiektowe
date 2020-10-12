05195393ae9e620fd756f3eefcf96e8d3da7093c
Kamil <61463685+CyberCommunism@users.noreply.github.com>
2020-10-12 17:14:49 +0200

    2.1

--- lab1/src/agh/cs/lab2/Vector2d.java
+++ lab1/src/agh/cs/lab2/Vector2d.java
@@ -19,41 +19,35 @@ public class Vector2d {
     //wypisanie
     public String toString()
     {
-        return (String)("("+Integer.toString(this.x)+","+Integer.toString(this.y)+")");
+        return "("+ this.x +","+this.y+")";
     }
 
     //porównanie
 
-    public boolean precedes(Vector2d other)
-    {
-        return other.x >= this.x && this.y <= other.y;
-    }
+    public boolean precedes(Vector2d other) { return other.x >= this.x && this.y <= other.y; }
+
     public boolean follows(Vector2d other)
     {
         return this.x >= other.x && this.y >= other.y;
     }
-    public Vector2d upperRight(Vector2d other)
-    {
-        return new Vector2d(Math.max(this.x, other.x), Math.max(this.y, other.y));
-    }
-    public Vector2d lowerLeft(Vector2d other)
-    {
-        return new Vector2d(Math.min(this.x, other.x), Math.min(this.y, other.y));
-    }
 
-    public Vector2d add(Vector2d other)
-    {
-        return new Vector2d(this.x+other.x,this.y+other.y);
-    }
-    public Vector2d subtract(Vector2d other)
-    {
-        return new Vector2d(this.x-other.x,this.y-other.y);
-    }
+    //rogi prostokata
+    public Vector2d upperRight(Vector2d other) { return new Vector2d(Math.max(this.x, other.x), Math.max(this.y, other.y)); }
 
+    public Vector2d lowerLeft(Vector2d other) { return new Vector2d(Math.min(this.x, other.x), Math.min(this.y, other.y)); }
+
+    //add/subs
+    public Vector2d add(Vector2d other) { return new Vector2d(this.x+other.x,this.y+other.y); }
+
+    public Vector2d subtract(Vector2d other) { return new Vector2d(this.x-other.x,this.y-other.y); }
+
+    //odbij
     public Vector2d opposite()
     {
         return new Vector2d((-this.x),(-this.y));
     }
+
+    //porownaj
     public boolean equals(Object other){
         if (this == other)
             return true;
